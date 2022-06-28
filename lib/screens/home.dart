bd43d5d57ebf1f435a45565c27b1f67b7543b448
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2022-06-29 00:04:54 +0530

    Read function of CRUD

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +19,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> datas =
+        FirebaseFirestore.instance.collection(widget.id).snapshots();
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -39,7 +42,48 @@ class _HomeState extends State<Home> {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            StreamBuilder<QuerySnapshot>(
+                stream: datas,
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                      content: Text("Some fuckn error"),
+                      duration: Duration(milliseconds: 1000),
+                    ));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  final List store = [];
+                  snapshot.data!.docs.map((DocumentSnapshot document) {
+                    Map a = document.data() as Map<String, dynamic>;
+                    store.add(a);
+                    a['id'] = document.id;
+                  }).toList();
+
+                  return Column(
+                    children: List.generate(
+                        store.length,
+                        (i) => Column(
+                              children: [
+                                Text(
+                                  store[i]['text'],
+                                ),
+                                SizedBox(
+                                  height: 20.0,
+                                ),
+                                Text(
+                                  store[i]['text'],
+                                ),
+                              ],
+                            )),
+                  );
+                }),
+          ],
         ),
       ),
     );
